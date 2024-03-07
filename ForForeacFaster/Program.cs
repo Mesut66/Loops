bc4d17b8c68d6e80882ae51b0344e7adc3d9edb1
Mesut <41824856+Mesut66@users.noreply.github.com>
2024-03-07 09:47:44 +0300

    Kod düzenlemesi yapıldı

--- ForForeacFaster/Program.cs
+++ ForForeacFaster/Program.cs
@@ -13,38 +13,37 @@ namespace ForForeacFaster
         {
 
             Context db = new Context();
-            var orders = db.orders.ToList();
+            var orderList = db.orders.ToList();
 
             Stopwatch stopwatch1 = new Stopwatch();
+
             stopwatch1.Start();
-            foreach (var item in orders)
+
+            for (int i = 0; i < orderList.Count; i++)
             {
-                Console.WriteLine("Foreach => Freight: {0}", item.Freight);
+                Console.WriteLine("For => Freight: {0}", orderList[i].Freight);
             }
 
             stopwatch1.Stop();
             TimeSpan ts1 = stopwatch1.Elapsed;
             Console.WriteLine("---------------------------------");
-            Console.WriteLine("Data Adedi: {0}", orders.Count);
+            Console.WriteLine("Data Adedi: {0}", orderList.Count);
+            Console.WriteLine("For Döngüsü Süresi: {0:00}:{1:00}:{2:00}:{3:00}", ts1.Hours, ts1.Minutes, ts1.Seconds, ts1.Milliseconds / 10);
 
-            Console.WriteLine("Foreach Döngüsü Süresi: {0:00}:{1:00}:{2:00}:{3:00}", ts1.Hours, ts1.Minutes, ts1.Seconds, ts1.Milliseconds / 10);
 
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+            foreach (var item in orderList)
+            {
+                Console.WriteLine("Foreach => Freight: {0}", item.Freight);
+            }
 
-            //Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Stop();
+            TimeSpan ts2 = stopwatch2.Elapsed;
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Data Adedi: {0}", orderList.Count);
 
-            //stopwatch2.Start();
-
-            //for (int i = 0; i < orders.Count; i++)
-            //{
-            //    Console.WriteLine("For => Freight: {0}", orders[i].Freight);
-            //}
-
-            //stopwatch2.Stop();
-            //TimeSpan ts2 = stopwatch2.Elapsed;
-            //Console.WriteLine("---------------------------------");
-            //Console.WriteLine("Data Adedi: {0}", orders.Count);
-            //Console.WriteLine("For Döngüsü Süresi: {0:00}:{1:00}:{2:00}:{3:00}", ts2.Hours, ts2.Minutes, ts2.Seconds, ts2.Milliseconds / 10);
-
+            Console.WriteLine("Foreach Döngüsü Süresi: {0:00}:{1:00}:{2:00}:{3:00}", ts2.Hours, ts2.Minutes, ts2.Seconds, ts2.Milliseconds / 10);
 
             Console.Read();
         }
